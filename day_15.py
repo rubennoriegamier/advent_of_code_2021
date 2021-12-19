4bed87870eb94fac5ac9075ead7abb6c6d088f1d
rubennoriegamier <rubennoriegamier@gmail.com>
2021-12-19 20:52:23 +0100

    Refactor day 15, part 2

--- day_15.py
+++ day_15.py
@@ -2,8 +2,9 @@ import fileinput
 from functools import cache, partial
 
 import networkx as nx
-
-OFFSETS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+import numpy as np
+# noinspection PyProtectedMember
+from numpy.lib.stride_tricks import as_strided
 
 
 def main():
@@ -32,39 +33,34 @@ def part_1(risk_levels: list[list[int]]) -> int:
     return min(solve(1, 0), solve(0, 1))
 
 
-def part_2(risk_levels: list[list[int]], scale: int = 1) -> int:
-    height = len(risk_levels)
-    width = len(risk_levels[0])
+def part_2(risk_levels: list[list[int]], scale: int) -> int:
+    risk_levels = np.asarray(risk_levels, dtype=np.byte)
+    height, width = risk_levels.shape
     scaled_height = height * scale
     scaled_width = width * scale
+    scaled_risk_levels = np.zeros((scaled_height + 2, scaled_width + 2), np.byte)
+    scaled_risk_levels[1:-1, 1:-1] = as_strided(np.arange(-1, scale - 1 << 1, dtype=np.byte),
+                                                (scale, height, scale, width), (1, 0, 1, 0)) \
+        .reshape((scaled_height, -1))
+
+    for y in range(0, scaled_height, height):
+        for x in range(0, scaled_width, width):
+            scaled_risk_levels[y + 1:y + height + 1, x + 1:x + width + 1] += risk_levels
+    scaled_risk_levels %= 9
+    scaled_risk_levels += 1
+
     graph = nx.DiGraph()
 
-    for y, row in enumerate(risk_levels):
-        for x, risk_level in enumerate(row):
-            for y_scale in range(scale):
-                for x_scale in range(scale):
-                    scaled_y = y + height * y_scale
-                    scaled_x = x + width * x_scale
-                    scaled_risk_level = (risk_level - 1 + y_scale + x_scale) % 9 + 1
+    for y in range(1, scaled_height + 1):
+        for x in range(1, scaled_width + 1):
+            yx = y, x
 
-                    for y_offset, x_offset in OFFSETS:
-                        scaled_neighbor_y = scaled_y + y_offset
-                        scaled_neighbor_x = scaled_x + x_offset
+            graph.add_edge(yx, (y - 1, x), weight=scaled_risk_levels[y - 1, x])
+            graph.add_edge(yx, (y + 1, x), weight=scaled_risk_levels[y + 1, x])
+            graph.add_edge(yx, (y, x - 1), weight=scaled_risk_levels[y, x - 1])
+            graph.add_edge(yx, (y, x + 1), weight=scaled_risk_levels[y, x + 1])
 
-                        if 0 <= scaled_neighbor_y < scaled_height and 0 <= scaled_neighbor_x < scaled_width:
-                            neighbor_y_scale, neighbor_y = divmod(scaled_neighbor_y, height)
-                            neighbor_x_scale, neighbor_x = divmod(scaled_neighbor_x, width)
-                            neighbor_risk_level = risk_levels[neighbor_y][neighbor_x]
-                            scaled_neighbor_risk_level = (neighbor_risk_level - 1
-                                                          + neighbor_y_scale
-                                                          + neighbor_x_scale) % 9 + 1
-
-                            graph.add_edge((scaled_y, scaled_x), (scaled_neighbor_y, scaled_neighbor_x),
-                                           weight=scaled_neighbor_risk_level)
-                            graph.add_edge((scaled_neighbor_y, scaled_neighbor_x), (scaled_y, scaled_x),
-                                           weight=scaled_risk_level)
-
-    return nx.shortest_path_length(graph, (0, 0), (scaled_height - 1, scaled_width - 1), 'weight')
+    return nx.shortest_path_length(graph, (1, 1), (scaled_height, scaled_width), 'weight')
 
 
 if __name__ == '__main__':
