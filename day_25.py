76fcbd5086642e52e5e3ca259bfc9f4862f33f2a
rubennoriegamier <rubennoriegamier@gmail.com>
2021-12-30 20:44:11 +0100

    Day 25: use only one mask

--- day_25.py
+++ day_25.py
@@ -18,27 +18,28 @@ def parse_seafloor_map(raw_seafloor_map):
 
 
 def part_1(seafloor_map):
-    east_mask = np.empty_like(seafloor_map, bool)
-    south_mask = np.empty_like(seafloor_map, bool)
+    mask = np.empty_like(seafloor_map, bool)
 
     for step in count(1):
         # East
-        np.equal(seafloor_map, 0, out=east_mask)
-        np.equal(seafloor_map[:, :-1], 1, out=east_mask[:, 1:], where=east_mask[:, 1:])
-        np.equal(seafloor_map[:, -1], 1, out=east_mask[:, 0], where=east_mask[:, 0])
-        seafloor_map[:, :-1][east_mask[:, 1:]] = 0
-        seafloor_map[:, -1][east_mask[:, 0]] = 0
-        seafloor_map[east_mask] = 1
+        np.equal(seafloor_map, 0, out=mask)
+        np.equal(seafloor_map[:, :-1], 1, out=mask[:, 1:], where=mask[:, 1:])
+        np.equal(seafloor_map[:, -1], 1, out=mask[:, 0], where=mask[:, 0])
+        seafloor_map[:, :-1][mask[:, 1:]] = 0
+        seafloor_map[:, -1][mask[:, 0]] = 0
+        seafloor_map[mask] = 1
+
+        any_ = mask.any()
 
         # South
-        np.equal(seafloor_map, 0, out=south_mask)
-        np.equal(seafloor_map[:-1], 2, out=south_mask[1:], where=south_mask[1:])
-        np.equal(seafloor_map[-1], 2, out=south_mask[0], where=south_mask[0])
-        seafloor_map[:-1][south_mask[1:]] = 0
-        seafloor_map[-1][south_mask[0]] = 0
-        seafloor_map[south_mask] = 2
+        np.equal(seafloor_map, 0, out=mask)
+        np.equal(seafloor_map[:-1], 2, out=mask[1:], where=mask[1:])
+        np.equal(seafloor_map[-1], 2, out=mask[0], where=mask[0])
+        seafloor_map[:-1][mask[1:]] = 0
+        seafloor_map[-1][mask[0]] = 0
+        seafloor_map[mask] = 2
 
-        if not east_mask.any() and not south_mask.any():
+        if not any_ and not mask.any():
             return step
 
 
